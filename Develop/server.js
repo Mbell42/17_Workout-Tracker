8ebd913fbdfe85eb4b09f38c62825b2d7bd64b1a
Matthew Bell <matthewbell1030@gmail.com>
2020-05-02 21:24:06 -0700

    added dependencies and middleware code to 'server.js'

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,29 @@
 // DEPENDENCIES
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
 
 // PORT
+const PORT = process.env.PORT || 3000;
+
 
 // MIDDLEWARE
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 // CONNECT TO MONGO DB W/MONGOOSE
+const mongoose.connect();
 
 // ROUTES
 
+
 // START SERVER
+applicationCache.addEventListener(PORT, () => {
+
+});
